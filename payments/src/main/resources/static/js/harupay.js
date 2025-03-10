c682717ab997065c979bbd7224cf1381cbbc5a21
pjh612 <pjh_jn@naver.com>
2025-03-11 01:48:14 +0900

    payments - 결제 창 open 시 결제 준비할 수 있도록 SDK 수정

--- payments/src/main/resources/static/js/harupay.js
+++ payments/src/main/resources/static/js/harupay.js
@@ -6,17 +6,19 @@ class HaruPaySDK {
         this.clientId = clientId;
         this.baseUrl = 'http://payments:8071';
         this.successUrl = successUrl;
-        this.failureUrl = failureUrl;
     }
 
     // 결제창 열기
     open(options) {
-        const {orderId, paymentId, productName, amount, onSuccess, onError} = options;
-        const paymentUrl = `${this.baseUrl}/pay/${paymentId}`;
-
+        const {orderId, productName, amount} = options;
+        const self = this;
+        let popup;
         $.ajax({
             type: "post",
-            url: "/api/payment/prepare",
+            headers: {
+                "X-PAY-CLIENT-ID": this.clientId
+            },
+            url: `${this.baseUrl}/api/payment/prepare`,
             contentType: "application/json; charset=utf-8",
             data: JSON.stringify({
                 "orderId": orderId,
@@ -24,29 +26,29 @@ class HaruPaySDK {
                 "productName": productName
             }),
             success: function (data) {
-                const popup = window.open(
+                const paymentId = data.paymentId;
+                const paymentUrl = `${self.baseUrl}/pay/${paymentId}`;
+                popup = window.open(
                     paymentUrl,
                     "paymentWindow",
                     "width=500,height=700,scrollbars=no,resizable=no"
                 );
 
-                window.addEventListener("message", (event) => {
-                    const data = event.data;
-                    const url = new URL(this.successUrl);
-                    url.searchParams.append('orderId', data.orderId);
-                    url.searchParams.append('paymentId', data.requestId);
-                    url.searchParams.append('requestPrice', data.requestPrice);
-                    popup.location.href = url.toString();
-                });
+
             },
             error: function (e) {
                 alert("결제에 실패했습니다.");
             }
         });
 
-
-
-
+        window.addEventListener("message", (event) => {
+            const data = event.data;
+            const url = new URL(this.successUrl);
+            url.searchParams.append('orderId', data.orderId);
+            url.searchParams.append('paymentId', data.requestId);
+            url.searchParams.append('requestPrice', data.requestPrice);
+            popup.location.href = url.toString();
+        });
     }
 }
 
