26d219bf990331560703c4b4868b86905df68bc1
pjh612 <pjh_jn@naver.com>
2025-04-07 23:27:23 +0900

    Redis Lock transactionalEventListener 방식 채택

--- money/src/main/java/com/haru/money/common/lock/RedisLockAspect.java
+++ money/src/main/java/com/haru/money/common/lock/RedisLockAspect.java
@@ -7,10 +7,13 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.lang.reflect.Method;
 
@@ -20,11 +23,11 @@ import java.lang.reflect.Method;
 public class RedisLockAspect {
     private final RedissonClient redissonClient;
     private final ExpressionParser parser;
-    private final RequiresNewJoinPointExecutor joinPointExecutor;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public RedisLockAspect(RedissonClient redissonClient, RequiresNewJoinPointExecutor joinPointExecutor) {
+    public RedisLockAspect(RedissonClient redissonClient, ApplicationEventPublisher applicationEventPublisher) {
         this.redissonClient = redissonClient;
-        this.joinPointExecutor = joinPointExecutor;
+        this.applicationEventPublisher = applicationEventPublisher;
         this.parser = new SpelExpressionParser();
     }
 
@@ -37,17 +40,12 @@ public class RedisLockAspect {
         RLock lock = redissonClient.getLock(lockKey);
         try {
             if (lock.tryLock(redisLock.waitTime(), redisLock.leaseTime(), redisLock.unit())) {
-                return joinPointExecutor.execute(joinPoint);
+                return joinPoint.proceed();
             } else {
                 throw new RuntimeException("already locked");
             }
-        } catch (Exception e) {
-            throw e;
         } finally {
-            if (log.isTraceEnabled()) {
-                log.trace("Releasing lock for key: {}", lockKey);
-            }
-            lock.unlock();
+            applicationEventPublisher.publishEvent(new UnLockEvent(lock));
         }
     }
 
@@ -59,5 +57,17 @@ public class RedisLockAspect {
             }
         }
         return parser.parseExpression(keyExpression).getValue(context, String.class);
+    }
+
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void unlock(UnLockEvent event) {
+        if (log.isTraceEnabled()) {
+            log.trace("Releasing lock for key: {}", event.lock.getName());
+        }
+        event.lock.unlock();
+    }
+
+    public record UnLockEvent(RLock lock) {
     }
 }
