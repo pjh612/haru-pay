5d74cc3c8fea72201671fa7f193b0db1f93471b8
pjh612 <pjh_jn@naver.com>
2025-05-22 00:44:14 +0900

    money - 머니 충전 최종 실패 시 실패 이벤트 발행

--- money/src/main/java/com/haru/money/adapters/in/event/loadmoney/handler/LoadMoneyFinishedEventHandler.java
+++ money/src/main/java/com/haru/money/adapters/in/event/loadmoney/handler/LoadMoneyFinishedEventHandler.java
@@ -1,9 +1,12 @@
 package com.haru.money.adapters.in.event.loadmoney.handler;
 
+import com.haru.common.RequiresNewExecutor;
+import com.haru.money.adapters.in.event.loadmoney.LoadMoneyFinishedEvent;
 import com.haru.money.adapters.in.event.loadmoney.payload.FirmBankingFinishedPayload;
 import com.haru.money.application.usecase.LoadMoneyEdaUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,18 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class LoadMoneyFinishedEventHandler {
     private final LoadMoneyEdaUseCase loadMoneyUseCase;
+    private final ApplicationEventPublisher eventPublisher;
+    private final RequiresNewExecutor requiresNewExecutor;
 
     @Transactional
     public void handle(UUID sagaId, FirmBankingFinishedPayload payload) {
-        loadMoneyUseCase.loadMoney(sagaId);
+        try {
+            loadMoneyUseCase.loadMoney(sagaId);
+        } catch (Exception e) {
+            requiresNewExecutor.execute(() -> {
+                LoadMoneyFinishedEvent failEvent = LoadMoneyFinishedEvent.fail(payload.getLoadMoneyRequestId());
+                eventPublisher.publishEvent(failEvent);
+            });
+        }
     }
 }
